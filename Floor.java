6b03b4cf4b746b447fb0451e628877e813da7c57
Kunal Patil <kunal.kv1dr64@gmail.com>
2021-09-27 10:55:46 +0530

    Floor problem

--- Floor.java
+++ Floor.java
@@ -1,2 +1,30 @@
-package com.company.DSA__JAVA;public class Floor {
+package com.company.DSA__JAVA;
+
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr = {3, 13, 25, 46, 68, 72, 89, 90};
+        int target = 12;
+        System.out.println(floor(arr, target));
+    }
+
+//    Greatest no. less than equal to target
+    static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid =  start + (end  - start) / 2 ;
+
+            if (target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return end;
+    }
 }
