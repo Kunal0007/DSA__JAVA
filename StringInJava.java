d92839947aed9f8adfc03c214fca0d36aaee6529
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-23 12:35:07 +0530

    Strings In Java

---
+++ StringInJava.java
@@ -0,0 +1,26 @@
+package com.company.DSA__JAVA;
+
+import java.util.Arrays;
+
+public class StringInJava {
+    public static void main(String[] args) {
+        String a = "Kunal Patil";
+        String b = a;
+        System.out.println(a);
+        System.out.println(b);
+
+        String c = "Kunal";
+        String d = "Kunal";
+        System.out.println(c==d);
+
+        String e = new String("Kunal");
+        System.out.println(e==d);
+
+        System.out.println(d.equals(e));
+
+        System.out.println(07);
+        System.out.println("Kunal");
+        System.out.println(new  int[]{1,2,3,4,5});
+        System.out.println(Arrays.toString(new  int[]{1,2,3,4,5}));
+    }
+}
