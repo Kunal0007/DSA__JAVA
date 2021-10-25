a8a26ee90194c2e3b5c7fa46f9d7db2b603addf2
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-25 21:47:08 +0530

    Star Pattern 4

--- StarPatterns.java
+++ StarPatterns.java
@@ -4,7 +4,8 @@ public class StarPatterns {
     public static void main(String[] args) {
 //        pattern1(4);
 //        pattern2(4);
-        pattern3(4);
+//        pattern3(4);
+        patter4(4);
     }
 
     static void pattern1(int n) {
@@ -29,6 +30,15 @@ public class StarPatterns {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void patter4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
