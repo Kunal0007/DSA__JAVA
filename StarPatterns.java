6cd2f77890e07106b9571fe5067fb0d9bd705e9e
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-25 22:20:03 +0530

    Star Pattern 5

--- StarPatterns.java
+++ StarPatterns.java
@@ -5,7 +5,8 @@ public class StarPatterns {
 //        pattern1(4);
 //        pattern2(4);
 //        pattern3(4);
-        patter4(4);
+//        pattern4(4);
+        pattern5(4);
     }
 
     static void pattern1(int n) {
@@ -35,10 +36,26 @@ public class StarPatterns {
         }
     }
 
-    static void patter4(int n) {
+    static void pattern4(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int c;
+            if (i > n){
+                c = 2 * n - i;
+            }
+            else {
+                c = i;
+            }
+            for (int j = 1; j <= c; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
