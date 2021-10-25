e9ef5ef25feee1e35be815861df3ca94d42de1b0
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-25 21:36:52 +0530

    Star Pattern 3

---
+++ StarPatterns.java
@@ -0,0 +1,36 @@
+package com.company.DSA__JAVA;
+
+public class StarPatterns {
+    public static void main(String[] args) {
+//        pattern1(4);
+//        pattern2(4);
+        pattern3(4);
+    }
+
+    static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1 ; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
