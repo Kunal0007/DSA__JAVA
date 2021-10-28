112120f2445bb5a7a25424ede813f6f367097257
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-28 23:18:40 +0530

    Number Pattern 8

--- NumberPattern.java
+++ NumberPattern.java
@@ -7,7 +7,8 @@ public class NumberPattern {
 //        pattern3(4);
 //        pattern4(5);
 //        pattern6(4);
-        pattern7(4);
+//        pattern7(4);
+        pattern8(4);
     }
 
     static void pattern1(int n) {
@@ -87,6 +88,22 @@ public class NumberPattern {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i + 1 ; j++) {
                 System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n) {
+        for (int i = 1; i <= n ; i++) {
+            int space = n - i;
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1 ; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
