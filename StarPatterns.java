49ad26a238a5268eda7f30a81607fffd1dc52837
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-29 23:46:33 +0530

    Star Pattern 10

--- StarPatterns.java
+++ StarPatterns.java
@@ -10,8 +10,9 @@ public class StarPatterns {
 //        pattern6(4);
 //        pattern7(4);
 //        pattern8(4);
-        pattern9(4);
-//        pattern10(4);
+//        pattern9(4);
+        pattern10(4);
+//        pattern11(4);
     }
 
     static void pattern1(int n) {
@@ -110,8 +111,20 @@ public class StarPatterns {
             System.out.println();
         }
     }
-    
+
     static void pattern10(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int space = 1; space <= i - 1; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern11(int n) {
         for (int i = 1; i <= 2 * n - 1; i++) {
             int star = i > n ? 2 * n - i : i;
             int space = n - star;
