c597af14af2832562cf117e983ae0d7be7168033
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-12-26 18:05:22 +0530

    Changes in Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 package com.company.DSA__JAVA;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        LL list = new LL();
+        list.insertAtFirst(1);
+        list.insertAtFirst(2);
+        list.insertAtFirst(3);
+        list.insertAtFirst(4);
+        list.displayList();
+        list.insertAtEnd(0);
+        list.displayList();
+        list.insertAtBtw(23,2);
+        list.displayList();
+        System.out.println(list.deleteAtFirst());
+        list.displayList();
+        System.out.println(list.deleteAtLast());
+        list.displayList();
+        System.out.println(list.deleteAtBtw(2));
+        list.displayList();
     }
 }
