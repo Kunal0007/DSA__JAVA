49fb9b028debbb3beaf3c8c3478a88a63a2ba5bd
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-12-28 12:05:32 +0530

    Traversal Methods on Binary Tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -18,7 +18,7 @@ public class BinaryTree {
         root = null;
     }
 
-    void traversalInorder(Node root){
+    void traversalInorder(Node root){   // 4 2 5 1 7 3
         if (root == null){
            return;
         }
@@ -29,7 +29,7 @@ public class BinaryTree {
 
     }
 
-    void traversalPostorder(Node root){
+    void traversalPostorder(Node root){    // 4 5 2 7 3 1
         if (root == null){
             return;
         }
@@ -42,18 +42,34 @@ public class BinaryTree {
 
     }
 
+    void traversalPreorder(Node root){    // 1 2 4 5 3 7
+        if (root == null){
+            return;
+        }
+
+        System.out.print(root.value + "-");
+
+        traversalPreorder(root.left);
+
+        traversalPreorder(root.right);
+
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
 
         tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
+        tree.root.right.left = new Node(7);
         tree.root.left.right = new Node(5);
         tree.root.left.left = new Node(4);
 
         tree.traversalInorder(tree.root);
         System.out.println();
         tree.traversalPostorder(tree.root);
+        System.out.println();
+        tree.traversalPreorder(tree.root);
     }
 
 }
