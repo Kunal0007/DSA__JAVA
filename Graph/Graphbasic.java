a81fa508d2f6ad338db5c261fa685d17254f2aba
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-01-09 23:32:53 +0530

    Graph In Java

---
+++ Graph/Graphbasic.java
@@ -0,0 +1,49 @@
+package com.company.DSA__JAVA.Graph;
+
+public class Graphbasic {
+    int vertices;
+    boolean[][] adjMatrix;
+
+    public Graphbasic(int vertices){
+        this.vertices = vertices;
+        adjMatrix = new boolean[vertices][vertices];
+    }
+
+    public void addEdge(int firstV, int SecondV){
+        adjMatrix[firstV][SecondV] = true;
+        adjMatrix[SecondV][firstV] = true;
+    }
+
+    public void removeEdge(int firstV, int SecondV){
+        adjMatrix[firstV][SecondV] = false;
+        adjMatrix[SecondV][firstV] = false;
+    }
+
+   public void printList(){
+       for (int i = 0; i < vertices; i++) {
+           for (int j = 0; j < vertices; j++) {
+               if (adjMatrix[i][j]){
+                   System.out.print("1 ");
+               }
+               else {
+                   System.out.print("0 ");
+               }
+           }
+           System.out.println();
+       }
+   }
+
+    public static void main(String[] args) {
+        Graphbasic G = new Graphbasic(4);
+
+        G.addEdge(0, 1);
+        G.addEdge(0, 2);
+        G.addEdge(1, 2);
+        G.addEdge(2, 0);
+        G.addEdge(2, 3);
+
+        G.printList();
+
+    }
+
+}
