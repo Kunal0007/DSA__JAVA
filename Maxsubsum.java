5b29cdcd2b1a071cadb0bac5706278fe4343dfbd
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-08-25 23:52:49 +0530

    Maximum sum of subset of array

---
+++ Maxsubsum.java
@@ -0,0 +1,18 @@
+package com.company.DSA__JAVA;
+
+public class Maxsubsum {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,3,-8,7,-1,2,3};
+        System.out.println(maxsum(arr));
+    }
+
+    private static int maxsum(int[] arr) {
+        int res = arr[0];
+        int maxEnd = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            maxEnd = Math.max(maxEnd + arr[i], arr[i]);
+            res = Math.max(res, maxEnd);
+        }
+        return res;
+    }
+}
